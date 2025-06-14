5577f110a9468f80090a95c1af2058553fa26a61
ac3h00d <64670782+ac3h00d@users.noreply.github.com>
2025-06-14 13:27:40 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: '#000', height: '100vh', color: '#00FF41', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>Veal Swap Wallet Connected UI Coming Soon</h1>
+    </div>
+  );
+}
