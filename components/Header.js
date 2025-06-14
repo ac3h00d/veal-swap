8423056be43f3e06cf957919367b03a3ea564b5c
ac3h00d <64670782+ac3h00d@users.noreply.github.com>
2025-06-14 16:59:02 -0400

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header style={{
+      textAlign: 'center',
+      marginBottom: '40px',
+      borderBottom: '1px solid #00FF41',
+      paddingBottom: '20px'
+    }}>
+      <h1 style={{ color: '#00FF41', fontFamily: 'monospace' }}>
+        Veal Swap | Matrix Tracker
+      </h1>
+    </header>
+  );
+}
