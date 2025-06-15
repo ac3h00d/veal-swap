f752fc23110aef193d087e0d5ba52fe40ddaa7f4
ac3h00d <64670782+ac3h00d@users.noreply.github.com>
2025-06-14 23:08:48 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
-import Head from 'next/head';
+import React from 'react';
 import Header from '../components/Header';
 import TokenGrid from '../components/TokenGrid';
 
 export default function Home() {
   return (
-    <div style={{ backgroundColor: 'black', minHeight: '100vh', color: 'lime', padding: '20px' }}>
-      <Head>
-        <title>Veal Swap</title>
-        <meta name="description" content="Matrix-style token monitor powered by Solana" />
-        <link rel="icon" href="/veal_swap_icon_512.png" />
-      </Head>
-
+    <div style={{ margin: 0, padding: 0, backgroundColor: 'black' }}>
       <Header />
       <TokenGrid />
     </div>
