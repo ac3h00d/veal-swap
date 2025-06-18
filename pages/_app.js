3be6e8707eee70be409ec56ae252267d49c7d3cf
ac3h00d <64670782+ac3h00d@users.noreply.github.com>
2025-06-18 16:29:29 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
+// pages/_app.js
 import '../styles.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
-} 
-Added _app.js to enable global styles
+}
